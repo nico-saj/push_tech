1f9cd03f863fd44a417bf98016e17fdc5940c339
Mykola Saienko <nicolas.ozz.com@gmail.com>
2020-08-17 22:44:15 +0300

    Working realization

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,44 @@
+class NotificationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def push
+    @notification = Notification.new(notification_params)
+    @notification.save
+    p send_message
+  end
+
+  private
+
+  def send_message
+    Webpush.payload_send(
+      message: message,
+      endpoint: @notification.endpoint,
+      p256dh: @notification.p256dh,
+      auth: @notification.auth,
+      vapid: {
+        subject: 'mailto:mykola.saienko@rubygarage.org',
+        public_key: ENV['VAPID_PUBLIC_KEY'],
+        private_key: ENV['VAPID_PRIVATE_KEY']
+      },
+      ssl_timeout: 5,
+      open_timeout: 5,
+      read_timeout: 5
+    )
+  end
+
+  def jsonbody
+    @jsonbody ||= JSON.parse(request.body.read)
+  end
+
+  def notification_params
+    {
+      endpoint: jsonbody["subscription"]["endpoint"],
+      p256dh: jsonbody["subscription"]["keys"]["p256dh"],
+      auth: jsonbody["subscription"]["keys"]["auth"]
+    }
+  end
+
+  def message
+    jsonbody["message"]
+  end
+end
